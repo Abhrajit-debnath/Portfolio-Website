5a0de56024d2f194a3aa4f266945f0f4f21a1d47
Abhrajit Debnath <137435505+Abhrajit-debnath@users.noreply.github.com>
2025-03-02 11:37:05 +0530

    command change

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
