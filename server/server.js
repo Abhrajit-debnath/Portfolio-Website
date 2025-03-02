aa61963654d7b87f7177bfd2a710a3cc84febaa5
Abhrajit Debnath <137435505+Abhrajit-debnath@users.noreply.github.com>
2025-03-02 11:46:23 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;  
+
 
 app.use(cors());
 app.use(express.json());
