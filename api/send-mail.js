dc39b93a565c56c54f1ef6854d6a73d87c261b20
Abhrajit Debnath <137435505+Abhrajit-debnath@users.noreply.github.com>
2025-04-01 13:42:24 +0530

    do change in comments

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,52 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import nodemailer from "nodemailer";
-// import dotenv from "dotenv";
-// dotenv.config();
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(cors());
-// app.use(express.json());
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS,
-//   },
-// });
-// app.post("/send-mail", async (req, res) => {
-//   console.log("Received request:", req.body);
-
-//   const { firstName, lastName, email, mobileNumber, message } = req.body;
-//   if (!firstName || !lastName || !email || !mobileNumber || !message) {
-//     return res.status(400).json({ error: "All Fields Required" });
-//   }
-
-//   const mailoptions = {
-//     from: process.env.EMAIL_USER,
-//     replyTo: email,
-//     to: process.env.EMAIL_USER,
-//     subject: `New Enquiry from ${firstName} ${lastName}`,
-//     text: `📩 Sender Email : ${email}\n📞 Phone: ${mobileNumber}\n\n💬 Message:${message}`,
-//   };
-
-//   try {
-//     const info = await transporter.sendMail(mailoptions);
-//     console.log("Email sent successfully:", info.response);
-//     res.json({ success: true, message: "Email sent successfully!" });
-//   } catch (error) {
-//     console.error("Email sending failed:", error);
-//     res
-//       .status(500)
-//       .json({ error: "Failed to send email", details: error.message });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log("server is listening at port", PORT);
-// });
-
 import express from "express";
 import cors from "cors";
 import nodemailer from "nodemailer";
@@ -81,13 +32,11 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  debug: true, // ✅ Enable debugging
-  logger: true, // ✅ Log sending process
+  debug: true,
+  logger: true, 
 });
 
 app.post("/api/send-mail", async (req, res) => {
-  console.log("Received request:", req.body);
-
   const { firstName, lastName, email, mobileNumber, message } = req.body;
   if (!firstName || !lastName || !email || !mobileNumber || !message) {
     return res.status(400).json({ error: "All Fields Required" });
